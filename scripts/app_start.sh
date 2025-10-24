5c06970e97d4084779d9f27e6c41b2237b163fce
Lorand Sarkozi <lorand.sarkozi@codexworks.com>
2025-10-24 19:17:23 +0300

    fourth commit

--- scripts/app_start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Make sure our app files are readable
-chown -R nginx:nginx /usr/share/nginx/html || true
-
-# Start/restart nginx
-systemctl restart nginx
-
